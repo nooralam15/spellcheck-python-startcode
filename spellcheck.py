103b7f0f9e617b6385290a80b105b38291850238
nooralam15 <nooralam14@icloud.com>
2022-02-18 14:39:50 -0700

    Update spellcheck.py

--- spellcheck.py
+++ spellcheck.py
@@ -10,10 +10,7 @@ def main():
     # Load data files into lists
     dictionary = loadWordsFromFile("data-files/dictionary.txt")
     aliceWords = loadWordsFromFile("data-files/AliceInWonderLand.txt")
-
-    # Print first 50 values of each list to verify contents
-    print(dictionary[0:50])
-    print(aliceWords[0:50])
+    
 # end main()
 
 
@@ -26,6 +23,42 @@ def loadWordsFromFile(fileName):
     # Split text by one or more whitespace characters
     return re.split('\s+', textData)
 # end loadWordsFromFile()
+
+
+#initalize a function that will linear search the dictionary
+def linearDict():
+
+
+
+#intitialize a function that will display the menu
+def menu():
+    #initialize variable
+    loop = True
+    #create a while loop that will loop through the menu
+    while loop:
+        #print the the title of the menu
+        print("Main Menu")
+        userInp = input("Enter the number \n 1. Spell Check a Word (Linear Search) \n 2. Spell Check a Word (Binary Search) \n 3. Spell Check Alice In Wonderland (Linear Search) \n 4. Spell Check Alice In Wonderland (Binary Search) \n 5. Exit\n")
+        
+        if userInp == "1":
+            linearDict()
+
+        elif userInp == "2":
+            binaryDict()
+
+        elif userInp == "3":
+            linearAlice()
+
+        elif userInp == "4":
+            binaryAlice()
+
+        elif userInp == "5":
+            print("Program closed")
+            sys.exit()
+
+        else:
+            print("Invalid Response")
+menu()
 
 
 # Call main() to begin program
